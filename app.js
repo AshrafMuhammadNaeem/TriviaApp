69c55c062157079f603d3c5405b10f0111bb6297
Naeem Ashraf <naeemashraf.tud@gmail.com>
2021-08-17 16:21:07 +0500

    Created app.js file and added the question and answer choices

---
+++ app.js
@@ -0,0 +1,14 @@
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+    question.innerHTML = "What is name of Pakistan's Capital City?"
+var choiceA = document.getElementById("choiceA");
+    choiceA.innerHTML = "Dera Ghazi Khan";
+var choiceB = document.getElementById("choiceB");
+    choiceB.innerHTML = "Karachi";
+var choiceC = document.getElementById("choiceC");
+    choiceC.innerHTML = "Islamabad";
+var choiceD = document.getElementById("choiceD");
+    choiceD.innerHTML = "Multan";
+
+      
+    
