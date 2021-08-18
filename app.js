bbdf4a07b9b245f2102a504ee4eee82c5621f912
Naeem Ashraf <naeemashraf.tud@gmail.com>
2021-08-18 13:56:28 +0500

    updating html and creating JS file with default questions and variables

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
+//Store questions in an array named questions
+var questions = [];
+//Store user score for each question in an array named points
+var score = 0;
+var points = [];
+//Store a default set of 5 questions in an array named defaultQuestions
+var defaultQuestions = [
+    {
+        question: "Where are the three smallest bones in the human body?",
+        choiceA: "middle ear",
+        choiceB: "nose",
+        choiceC: "toes",
+        choiceD: "eyes",
+        correct: "A"
+    },
+    {
+        question: "What is the most abundant element in the Universe?",
+        choiceA: "Helium",
+        choiceB: "Oxygen",
+        choiceC: "Lithium",
+        choiceD: "Hydrogen",
+        correct: "D"
+    },
+    {
+        question: "Approximately how long does it take for light to travel from the Sun's surface to the Earth?",
+        choiceA: "8 days",
+        choiceB: "8 seconds",
+        choiceC: "8 minutes",
+        choiceD: "8 hours",
+        correct: "C"
+    },
+    {
+        question: "What is 10/2?",
+        choiceA: "5",
+        choiceB: "2",
+        choiceC: "8",
+        choiceD: "9",
+        correct: "A"
+    },
+    {
+        question: "Which planet has the most moons?",
+        choiceA: "Saturn",
+        choiceB: "Mars",
+        choiceC: "Jupiter",
+        choiceD: "Uranus",
+        correct: "C"
+    }];
+
+]
+
+
+
+
+
+
 var quiz = document.getElementById("quiz");
 var question = document.getElementById("question");
-    question.innerHTML = "What is name of Pakistan's Capital City?"
+    
 var choiceA = document.getElementById("choiceA");
-    choiceA.innerHTML = "Dera Ghazi Khan";
+    
 var choiceB = document.getElementById("choiceB");
-    choiceB.innerHTML = "Karachi";
+    
 var choiceC = document.getElementById("choiceC");
-    choiceC.innerHTML = "Islamabad";
+    
 var choiceD = document.getElementById("choiceD");
-    choiceD.innerHTML = "Multan";
+    
 
       
     
